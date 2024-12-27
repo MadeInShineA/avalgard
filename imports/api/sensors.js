89dd146bcfb643c5ac45a7f3d5f4abe90f9d6ff9
Simon Masserey <simon.masserey@edu.vs.ch>
2024-12-27 15:43:54 +0100

    Initialised database with some sample data

---
+++ imports/api/sensors.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+
+export const SensorsCollection = new Mongo.Collection('sensors');
--- server/main.js
+++ server/main.js
@@ -1,41 +1,140 @@
-import { Meteor } from 'meteor/meteor'
-import { LinksCollection } from '/imports/api/links'
+import { Meteor } from 'meteor/meteor';
+import { Random } from "meteor/random";
+import { PlantsCollection } from '/imports/api/plants';
+import { UsersCollection } from '/imports/api/users';
+import { ClimatesCollection } from '/imports/api/climates';
+import { SensorsCollection } from '/imports/api/sensors';
 
-async function insertLink({ title, url }) {
-  await LinksCollection.insertAsync({ title, url, createdAt: new Date() })
-}
+//Reset dbs: db.getCollectionNames().forEach(function(collectionName){if (!collectionName.startsWith("system.")) {db[collectionName].drop();}});
 
 Meteor.startup(async () => {
-  // If the Links collection is empty, add some data.
-  if ((await LinksCollection.find().countAsync()) === 0) {
-    await insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://vuejs.org/guide/quick-start.html',
-    })
-
-    await insertLink({
-      title: 'Follow the Guide',
-      url: 'https://guide.meteor.com',
-    })
-
-    await insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com',
-    })
-
-    await insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com',
-    })
-
-    await insertLink({
-      title: 'Test link',
-      url: 'https://smash-map.com',
-    })
-
-    await insertLink({
-      title: 'Test link 2',
-      url: 'https://google.com',
-    })
+  if ((await ClimatesCollection.find().countAsync()) === 0) {
+    await Promise.all([
+      ClimatesCollection.insertAsync({ 
+        name: 'Tempéré', 
+        description: 'Climat modéré avec quatre saisons distinctes',
+        temperatureRange: { min: 5, max: 25 }
+      }),
+      ClimatesCollection.insertAsync({ 
+        name: 'Méditerranéen', 
+        description: 'Climat chaud et sec en été, doux en hiver',
+        temperatureRange: { min: 10, max: 35 }
+      })
+    ]);
   }
-})
+
+  if ((await PlantsCollection.find().countAsync()) === 0) {
+    await Promise.all([
+      PlantsCollection.insertAsync({ 
+        name: 'Tomate',
+        maintenances: [
+          {
+            _id: Random.id(),
+            name: "Arrosage",
+            frequency: 5,
+          },
+          {
+            _id: Random.id(),
+            name: "Sulfatage",
+            frequency: 150,
+          }
+        ]
+      }),
+      PlantsCollection.insertAsync({ 
+        name: 'Basilic',
+        maintenances: [
+          {
+            _id: Random.id(),
+            name: "Arrosage",
+            frequency: 5,
+          },
+          {
+            _id: Random.id(),
+            name: "Anti-limaces",
+            frequency: 30,
+          }
+        ]
+      })
+    ]);
+  }
+
+  if ((await UsersCollection.find().countAsync()) === 0) {
+    await Promise.all([
+      UsersCollection.insertAsync({ 
+        name: 'Marco Paladini',
+        email: 'alice.dupont@email.com',
+        gardens: [
+          {
+            _id: Random.id(),
+            name: 'Jardin Principal',
+            climateId: 'id',
+            plants: [
+              {
+                _id: Random.id(),
+                plantId: 'id',
+                position: { x: 2, y: 2 },
+                sensors: [
+                  {
+                    _id: Random.id(),
+                    sensorId: 'id',
+                    datasUrl: "url"
+                  }
+                ],
+                maintenancesDone: [
+                  {
+                    _id: Random.id(),
+                    maintenanceId: "id",
+                    date: new Date()
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }),
+      UsersCollection.insertAsync({ 
+        name: 'Marc Bolan',
+        email: 'bob.martin@email.com',
+        gardens: [
+          {
+            _id: Random.id(),
+            name: 'Jardin Urbain',
+            climateId: 'id',
+            plants: [
+              {
+                _id: Random.id(),
+                plantId: 'id',
+                position: { x: 2, y: 2 },
+                sensors: [
+                  {
+                    _id: Random.id(),
+                    sensorId: 'id',
+                    datasUrl: "url"
+                  }
+                ],
+                maintenancesDone: [
+                  {
+                    _id: Random.id(),
+                    maintenanceId: "id",
+                    date: new Date()
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      })
+    ]);
+  }
+
+  if ((await SensorsCollection.find().countAsync()) === 0) {
+    await Promise.all([
+      SensorsCollection.insertAsync({
+        name: 'Capteur de Température',
+      }),
+      SensorsCollection.insertAsync({
+        name: 'Capteur Humidité',
+      })
+    ]);
+  }
+});
