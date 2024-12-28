d8d1b08d2954743dd159d94f815d829e384f772d
MadeInShineA <olivier.amacker@netplus.ch>
2024-12-28 13:42:14 +0100

    Added user login, register and logout (need to fix register error messages)

--- imports/ui/router.js
+++ imports/ui/router.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './Home.vue'
 import About from './About.vue'
+<<<<<<< HEAD
 import Tasks from './Tasks.vue'
 
+=======
+import LoginForm from './LoginForm.vue'
+import SignupForm from './SignupForm.vue'
+>>>>>>> 4b760ac (Added user login, register and logout (need to fix register error messages))
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -18,9 +23,20 @@ export const router = createRouter({
       component: About,
     },
     {
+<<<<<<< HEAD
       path: '/tasks',
       name: 'tasks',
       component: Tasks,
+=======
+      path: '/login',
+      name: 'login',
+      component: LoginForm
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupForm
+>>>>>>> 4b760ac (Added user login, register and logout (need to fix register error messages))
     }
   ],
 })
