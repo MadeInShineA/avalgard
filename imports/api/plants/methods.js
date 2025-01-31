0c068545749849b554bd5faf44f58242415ba52c
Simon Masserey <simon.masserey@edu.vs.ch>
2025-01-31 15:45:13 +0100

    Added two slider to filter the plants by growthDuration and wateringConditions

--- imports/api/plants/methods.js
+++ imports/api/plants/methods.js
@@ -15,6 +15,34 @@ Meteor.methods({
     const plants = await PlantsCollection.find({ name: { $regex: regex } }).fetch();
     return plants;
   },
+  'plants.searchAndFilter': async function (options = {}) {
+    const { 
+      searchQuery = '', 
+      minGrowthDuration = 0, 
+      maxGrowthDuration = 400, 
+      minWaterRequirement = 0, 
+      maxWaterRequirement = 5 
+    } = options;
+
+    const query = {};
+
+    if (searchQuery) {
+      query.name = { $regex: new RegExp(searchQuery, 'i') };
+    }
+
+    query.growthDuration = { 
+      $gte: minGrowthDuration, 
+      $lte: maxGrowthDuration 
+    };
+
+    query.waterRequirement = { 
+      $gte: minWaterRequirement, 
+      $lte: maxWaterRequirement 
+    };
+
+    const plants = await PlantsCollection.find(query).fetch();
+    return plants;
+  },
   'plants.findByName': async function (plantName) {
     check(plantName, String);
 
