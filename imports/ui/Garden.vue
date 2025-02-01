0b6e8110ad42df86d4ba6fdc77c7f75f13400f1b
lamacrym0 <lamastico2.0@gmail.com>
2025-02-01 22:31:12 +0100

    Add dark style and ripplebutton

--- imports/ui/Garden.vue
+++ imports/ui/Garden.vue
@@ -5,6 +5,7 @@ import { Random } from "meteor/random";
 import { useRoute, useRouter } from 'vue-router';
 import VueDraggableResizable from 'vue-draggable-resizable';
 import VueSlider from 'vue-3-slider-component';
+import RippleButton from './RippleButton.vue';
 
 const garden = ref(null)
 const climate = ref()
@@ -451,20 +452,21 @@ function viewTasks() {
           :style="{ background: 'none', border: 'none' }"
           >
           <div v-if="plant.isVisible" 
-            :style="{ position: 'absolute', inset: 0, backgroundColor: 'transparent', border: '3px solid black', 
-                      color: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center' }">
-            <img :src="'/plants_sprites/' + plant.sprite">
-            <p v-if="plant.w > 5 * CELL_SIZE">{{ plant.name.charAt(0).toUpperCase() + plant.name.slice(1) }}</p>
-            <button @click="openModificationModal(plant)"
-              class="absolute top-0 right-0 text-white rounded-full w-6 h-6 flex justify-center items-center bg-gray-600 hover:bg-gray-500">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" class="w-4 h-4">
-                <path
-                  d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
-                <path
-                  d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5Z" />
-              </svg>
-            </button>
-          </div>
+            :style="{position: 'absolute', inset: 0, backgroundColor: 'transparent', border: '3px solid black', 
+            color: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center' }"
+            >
+          <img :src="'/plants_sprites/' + plant.sprite">
+          <p v-if="plant.w > 5 * CELL_SIZE">{{ plant.name.charAt(0).toUpperCase() + plant.name.slice(1) }}</p>
+          <button @click="openModificationModal(plant)"
+            class="absolute top-0 right-0 text-white rounded-full w-6 h-6 flex justify-center items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" class="size-6">
+              <path
+                d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+              <path
+                d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+            </svg>
+          </button>
+        </div>
         </vue-draggable-resizable>
       </div>
 
@@ -513,12 +515,17 @@ function viewTasks() {
         </p>
       </div>
       <ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <li v-for="plant in plants" :key="plant._id" @click="addPlantToGarden(plant, isPlantClimateCompatible(plant))"
-          :class="['p-4 shadow-md rounded-lg cursor-pointer text-center',
-            isPlantClimateCompatible(plant) ? 'bg-green-100 hover:bg-green-200 dark:bg-green-900 dark:hover:bg-green-800' : 'bg-red-100 hover:bg-red-200 dark:bg-red-900 dark:hover:bg-red-800']">
-          <p class="font-bold text-green-900 dark:text-green-300">
+        <li v-for="plant in plants" :key="plant._id" class="w-full">
+          <RippleButton 
+            @click="addPlantToGarden(plant, isPlantClimateCompatible(plant))"
+            :class="[
+              'w-full font-bold shadow-md rounded-lg cursor-pointer text-center',
+              isPlantClimateCompatible(plant)
+                ? 'bg-green-100 hover:bg-green-200 dark:bg-green-900 dark:hover:bg-green-800 text-green-900 dark:text-green-300'
+                : 'bg-red-100 hover:bg-red-200 dark:bg-red-900 dark:hover:bg-red-800 text-red-900 dark:text-red-300'
+            ]">
             {{ plant.name.charAt(0).toUpperCase() + plant.name.slice(1) }}
-          </p>
+          </RippleButton>
         </li>
       </ul>
     </div>
