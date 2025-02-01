252c2285ce25cbbb43b0a452d7c79aed699bfc55
GDbateaux <yolan.savioz@hes-so.ch>
2025-02-01 22:14:52 +0100

    Change scroll-up button

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -7,12 +7,13 @@
     </main>
 
     <!-- Bouton de retour en haut -->
-    <button 
-      v-show="showScrollButton"
-      @click="scrollToTop"
-      class="fixed bottom-6 right-6 bg-green-600 text-white p-3 rounded-full shadow-md hover:bg-green-700 transition-opacity opacity-80 hover:opacity-100 transform hover:scale-110"
-    >
-      ⬆️
+    <button v-show="showScrollButton" @click="scrollToTop"
+      class="fixed bottom-6 right-6 bg-green-600 text-white p-3 rounded-full shadow-md hover:bg-green-700 transition-opacity opacity-80 hover:opacity-100 transform hover:scale-110">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#A7F4D0" class="size-6">
+        <path fill-rule="evenodd"
+          d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm.53 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v5.69a.75.75 0 0 0 1.5 0v-5.69l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
+          clip-rule="evenodd" />
+      </svg>
     </button>
 
     <Footer />
@@ -50,9 +51,11 @@ onUnmounted(() => {
 
 <style scoped>
 /* Empêche le déplacement du contenu quand la scrollbar apparaît */
-html, body {
+html,
+body {
   width: 100%;
-  overflow-x: hidden; /* Empêche tout scrolling horizontal */
+  overflow-x: hidden;
+  /* Empêche tout scrolling horizontal */
 }
 
 /* Utiliser `overflow: overlay` pour éviter le redimensionnement */
@@ -67,7 +70,9 @@ body {
 }
 
 /* Assure que l'application prend toute la hauteur de l'écran */
-html, body, #app {
+html,
+body,
+#app {
   min-height: 100vh;
 }
 
@@ -76,4 +81,3 @@ html, body, #app {
   flex: 1;
 }
 </style>
-
